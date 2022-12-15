2fb962e8f78fae39e674c6d97ebd5afc7f7969fe
Umer Siddique <ummersiddique@gmail.com>
2022-12-15 15:30:29 +0500

    Added native library

--- utils_library/src/main/java/com/example/utils_library/TestUtils.kt
+++ utils_library/src/main/java/com/example/utils_library/TestUtils.kt
@@ -2,6 +2,17 @@ package com.example.utils_library
 
 object TestUtils {
 
+    private external fun checkfopen(filepath: String): Boolean
+    private external fun runsu(command: String): Boolean
+    private external fun statfile(filepath: String): Boolean
+    private external fun runls(filepath: String, filename: String, exactMatch: Boolean): Boolean
+    private external fun runpmlist(packageName: String, exactMatch: Boolean): Boolean
+    private external fun checkifstream(filepath: String): Boolean
+
+    init {
+        System.loadLibrary("native2.cpp")
+    }
+
     fun testUtilsMethod() {
 
     }
